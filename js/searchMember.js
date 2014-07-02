b93292361547e977a8cd57aa30e2addb30df078b
cobayashi <sayakacobayashi@gmail.com>
2014-07-03 00:19:15 +0900

    Delete searchMember.js

--- js/searchMember.js
+++
@@ -1,7 +0,0 @@
-onload = function() {
-	$('form.formToRdf').submit(function(){return search($(".mem").val())});
-    function search(q) {
-        $.post('formToRdf.cgi',"member="+q);
-        return false;
-    }
-}
